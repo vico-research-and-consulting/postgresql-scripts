6eda8566cdc096fd5e3248a487a43743e84a03c7
Marc Schoechlin <marc.schoechlin@vico-research.com>
2020-02-05 07:59:57 +0100

    update

--- backup-databases.sh
+++ backup-databases.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+DUMPDIR="$1"
 BACKUPDIR="$1"
 MAXAGE="$2"
 
 TIMESTAMP="`date --date="today" "+%Y-%m-%d_%H-%M-%S"`"
 STARTTIME_GLOBAL="$SECONDS"
+BACKUP_TYPE="${BACKUP_TYPE:-custom}"
 
 sendStatus(){
     local STATUS="$1"
@@ -18,13 +20,41 @@ sendStatus(){
 
 if [ -z "$BACKUPDIR" ] || [ -z "$MAXAGE" ];then
 	echo "USAGE: $0 <BACKUP PATH> <MAXAGE IN DAYS>"
+	echo "       $0 <DUMP PATH>:<BACKUP PATH> <MAXAGE IN DAYS>"
 	exit 1
 fi
 
-cd $BACKUPDIR 
+if [ "$(whoami)" != "postgres" ];then
+  echo "INFO: not postgres, executing myself with sudo now"
+  exec sudo -u postgres $0 $@
+fi
+
+echo "INFO: adjusting /proc/$$/oom_score_adj to 1000"
+echo 1000 > /proc/$$/oom_score_adj
+
+if ( echo "$BACKUPDIR"|grep -q -P "/....+:/....+");then
+	DUMPDIR="$(echo "$BACKUPDIR"|cut -d ':' -f1)"
+	BACKUPDIR="$(echo "$BACKUPDIR"|cut -d ':' -f2)"
+  echo "INFO: a dumpdir was specified: $DUMPDIR"
+fi
+
+if [ ! -d "$BACKUPDIR" ];then
+    mkdir -p "$BACKUPDIR"
+fi
+
+if [ ! -d "$DUMPDIR" ];then
+    mkdir -p "$DUMPDIR"
+fi
+
+cd ${BACKUPDIR} 
 if [ "$?" != 0 ];then
-        echo "Unable to change to dir '$BACKUPDIR'"
+   echo "Unable to change to dir '${BACKUPDIR}'"
 	exit 1 
+fi
+
+if ( ! ( echo "$BACKUP_TYPE" |grep -q -P "custom|sql" ) );then
+   echo "Wrong backup type '$BACKUP_TYPE', use 'cusom' or 'sql'"
+   exit 1
 fi
 
 LOCK_FILE="/tmp/postgresql-backup.lock"
@@ -43,18 +73,43 @@ while read DBNAME;
 do
    echo "*** BACKUP $DBNAME ****************************************************************************"
    STARTTIME="$SECONDS"
-   pg_dump $DBNAME |
-	    gzip -c > ${DBNAME}-${TIMESTAMP}_currently_dumping.sql.gz && 
-	 mv ${DBNAME}-${TIMESTAMP}_currently_dumping.sql.gz ${DBNAME}-${TIMESTAMP}.sql.gz
-   RET="$?"
+
+   echo "=> backup schema"
+   pg_dump -c $DBNAME -s -f ${DUMPDIR}/${DBNAME}-${TIMESTAMP}_schema.sql.gz -Z 7
+   RET1="$?"
+
+   echo "=> backup database"
+
+   if [ "$BACKUP_TYPE" = "custom" ];then
+      pg_dump -Fc -c -f ${DUMPDIR}/${DBNAME}-${TIMESTAMP}_currently_dumping.custom.gz -Z 7 --inserts $DBNAME && 
+         mv ${DUMPDIR}/${DBNAME}-${TIMESTAMP}_currently_dumping.custom.gz ${DUMPDIR}/${DBNAME}-${TIMESTAMP}.custom.gz
+      RET2="$?"
+   else
+      pg_dump -f ${DUMPDIR}/${DBNAME}-${TIMESTAMP}_currently_dumping.sql.gz -Z 7 $DBNAME && 
+         mv ${DUMPDIR}/${DBNAME}-${TIMESTAMP}_currently_dumping.sql.gz ${DUMPDIR}/${DBNAME}-${TIMESTAMP}.sql.gz
+      RET2="$?"
+   fi
+
+   if [ "${DUMPDIR}" != "${BACKUPDIR}" ];then
+      echo "=> move backups to ${BACKUPDIR}"
+		mv -v ${DUMPDIR}/${DBNAME}-${TIMESTAMP}_schema.sql.gz ${BACKUPDIR}/${DBNAME}-${TIMESTAMP}_schema.sql.gz &&
+      if [ "$BACKUP_TYPE" = "custom" ];then
+         mv -v ${DUMPDIR}/${DBNAME}-${TIMESTAMP}.custom.gz ${BACKUPDIR}/${DBNAME}-${TIMESTAMP}.custom.gz
+      else
+         mv -v ${DUMPDIR}/${DBNAME}-${TIMESTAMP}.sql.gz ${BACKUPDIR}/${DBNAME}-${TIMESTAMP}.sql.gz
+      fi
+      RET3="$?"
+   else
+	  RET3="0"
+   fi
 
    DURATION="$(( $(( $SECONDS - $STARTTIME )) / 60 ))"
-   if [ "$RET" == "0" ];then
-        sendStatus "INFO: SUCESSFULLY CREATED BACKUP FOR '$DBNAME' in $DURATION minutes"
+   if ( [ "$RET1" == "0" ] && [ "$RET2" == "0" ] && [ "$RET3" == "0" ]);then
+        sendStatus "INFO: SUCESSFULLY CREATED BACKUP FOR '$DBNAME' after $DURATION minutes"
         SUCCESSFUL="$(( $SUCCESSFUL + 1))"
    else
      FAILED="$(($FAILED + 1))"
-        sendStatus "INFO: FAILED TO BACKUP '$DBNAME'  in $DURATION minutes"
+        sendStatus "ERROR: FAILED TO BACKUP '$DBNAME' after $DURATION minutes"
    fi
 done < <(psql -q -t -A -c "SELECT datname FROM pg_database WHERE datistemplate = false;")
 
@@ -68,10 +123,11 @@ fi
 echo "*** REMOVE OUTDATED BACKUPS **********************************************************************"
 
 if ( echo -n "$MAXAGE"|egrep -q '^[0-9][0-9]*$' );then
-	find $BACKUPDIR -name "*.sql.gz" -mtime +${MAXAGE} -exec rm -fv {} \;
+	find ${BACKUPDIR} -type f -name "*.custom.gz" -mtime +${MAXAGE} -exec rm -fv {} \;
+	find ${BACKUPDIR} -type f -name "*.sql.gz" -mtime +${MAXAGE} -exec rm -fv {} \;
 else
-	echo "Age not correctly defined"
+	echo "Age not correctly defined, '$MAXAGE'"
 	exit 1 
 fi
 
-echo "TOTAL AMOUNT OF BACKUPS $( du -scmh *.sql.gz|awk '/total/{print $1}')"
+echo "TOTAL AMOUNT OF BACKUPS $( du -scmh *.custom.gz|awk '/total/{print $1}')"
